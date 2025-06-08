9c5cae9e4e8fcda79180847d9a48a53fb045c5ce
rivka1532 <r71615.32@gmail.com>
2025-06-08 16:25:51 +0300

    08/06/2025

--- obj/Debug/net8.0/myApiProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/myApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c773e22c4339d6855a8fb52d23fa492757b30c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfbf97556e825e1bc733cac304c2f0a2c49911e")]
 [assembly: System.Reflection.AssemblyProductAttribute("myApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
